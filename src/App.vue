36d58d8cdd22f0a5ab34af7aab63cabefa3941a9
ldz59 <23553545@qq.com>
2022-09-24 11:26:30 +0800

    feat:完成多选删除功能

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,11 @@
     </div>
     <!-- 功能 -->
     <div class="query-box">
-      <el-input v-model="queryInput" placeholder="Please input" />
-      <el-button type="primary">增加</el-button>
+      <el-input class="query-input" v-model="queryInput" placeholder="Please input" />
+      <div class="btn-list">
+        <el-button type="primary" @click="handleAdd">增加</el-button>
+        <el-button type="danger" @click="handleDelList" v-if="multipleSelection.length>0">删除</el-button>
+      </div>
     </div>
     <!-- 表格 -->
     <el-table 
@@ -18,21 +21,50 @@
       border
     >
       <el-table-column type="selection" width="55" />
-      <el-table-column fixed prop="date" label="Date" width="150" />
-      <el-table-column prop="name" label="Name" width="120" />
-      <el-table-column prop="state" label="State" width="120" />
-      <el-table-column prop="city" label="City" width="120" />
-      <el-table-column prop="address" label="Address" width="600" />
-      <el-table-column prop="zip" label="Zip" width="120" />
-      <el-table-column fixed="right" label="Operations" width="120">
-        <template #default>
-          <el-button link type="primary" size="small" @click="handleRowClick"
-            >Detail</el-button
-          >
-          <el-button link type="primary" size="small">Edit</el-button>
+      <el-table-column prop="id" label="编号" width="60" />
+      <el-table-column prop="name" label="姓名" width="120" />
+      <el-table-column prop="email" label="邮箱" width="120" />
+      <el-table-column prop="phone" label="手机" width="120" />
+      <el-table-column prop="state" label="状态" width="120" />
+      <el-table-column prop="address" label="地址" width="300" />
+      <el-table-column fixed="right" label="操作" width="120">
+        <template #default="scope">
+          <el-button style="color:#f56c6c"
+          link
+          type="primary"
+          size="small"
+          @click="handleRowDel(scope.row)"
+        >删除</el-button>
+          <el-button link type="primary" size="small">编辑</el-button>
         </template>
       </el-table-column>
     </el-table>
+
+    <!-- dialog -->
+    <el-dialog v-model="dialogFormVisible" :title="dialogType === 'add' ? '新增' : '编辑'">
+      <el-form :model="tableForm">
+        <el-form-item label="姓名" :label-width="100">
+          <el-input v-model="tableForm.name" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="100">
+          <el-input v-model="tableForm.email" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="电话" :label-width="100">
+          <el-input v-model="tableForm.phone" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="状态" :label-width="100">
+          <el-input v-model="tableForm.state" autocomplete="off" />
+        </el-form-item>
+        <el-form-item label="地址" :label-width="100">
+          <el-input v-model="tableForm.address" autocomplete="off" />
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button type="primary" @click="dialogConfirm">确认</el-button>
+        </span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -44,62 +76,110 @@
   let queryInput = ref("")
   let tableData = ref([
     {
-      date: '2016-05-03',
-      name: 'Tom',
+      id: '1',
+      name: 'Tom1',
+      email: '123@qq.com',
+      phone: '13012345678',
       state: 'California',
-      city: 'Los Angeles',
       address: 'No. 189, Grove St, Los Angeles',
-      zip: 'CA 90036',
-      tag: 'Home',
     },
     {
-      date: '2016-05-02',
-      name: 'Tom',
+      id: '2',
+      name: 'Tom2',
+      email: '123@qq.com',
+      phone: '13012345678',
       state: 'California',
-      city: 'Los Angeles',
       address: 'No. 189, Grove St, Los Angeles',
-      zip: 'CA 90036',
-      tag: 'Office',
     },
     {
-      date: '2016-05-04',
-      name: 'Tom',
+      id: '3',
+      name: 'Tom3',
+      email: '123@qq.com',
+      phone: '13012345678',
       state: 'California',
-      city: 'Los Angeles',
       address: 'No. 189, Grove St, Los Angeles',
-      zip: 'CA 90036',
-      tag: 'Home',
     },
     {
-      date: '2016-05-01',
-      name: 'Tom',
+      id: '4',
+      name: 'Tom4',
+      email: '123@qq.com',
+      phone: '13012345678',
       state: 'California',
-      city: 'Los Angeles',
       address: 'No. 189, Grove St, Los Angeles',
-      zip: 'CA 90036',
-      tag: 'Office',
+    },
+    {
+      id: '5',
+      name: 'Tom5',
+      email: '123@qq.com',
+      phone: '13012345678',
+      state: 'California',
+      address: 'No. 189, Grove St, Los Angeles',
     },
   ])
   let multipleSelection = ref([])
+  let dialogFormVisible = ref(false)
+  let tableForm = ref({
+    name: '张三',
+    email: '123@qq.com',
+    phone: '13012345678',
+    state: '在职',
+    address: '福建省',
+  })
+  let dialogType = ref('add')
+
 
   // 方法
-  
-  const handleRowClick = () => {
-    console.log('click')
+  // 删除一条
+  const handleRowDel = ({id}) => {
+    console.log(id)
+    // 1. 通过ID获取到条目对应的 索引值
+    let index = tableData.value.findIndex(item=>item.id === id)
+    // 2. 通过索引值进行删除对应条目
+    tableData.value.splice(index,1)
   }
+  // 多条删除
+  const handleDelList = () => {
+    multipleSelection.value.forEach(id=>{
+      handleRowDel({id})
+    })
+    // 清空
+    multipleSelection.value = []
+  }
+  // 选中
   const handleSelectionChange = (val) => {
-    multipleSelection.value = val
+    // multipleSelection.value = val
+    // 清空
+    multipleSelection.value = []
+    val.forEach(item => {
+      multipleSelection.value.push(item.id)
+    })
+    console.log(multipleSelection.value);
+    
   }
-  
+  // 新增
+  const handleAdd = () => {
+    dialogFormVisible.value = true
+    tableForm.value = {}
+  }
+  // 确认
+  const dialogConfirm = () => {
+    dialogFormVisible.value = false
+    // 1.拿到数据
+
+    // 2.添加到表格
+    tableData.value.push({
+      id: (tableData.value.length + 1).toString(),
+      ...tableForm.value,
+    })
+    console.log(tableData);
+  }
+
 </script>
 
 <style scoped>
   .table-box {
     max-width: 800px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    margin: 200px;
   }
   .title {
     text-align: center;
@@ -109,7 +189,7 @@
     justify-content: space-between;
     margin-bottom: 20px;
   }
-  .el-input {
+  .query-input {
     width:200px
   }
 </style>
